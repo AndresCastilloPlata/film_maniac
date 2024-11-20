aa838edef6174ad83d3ce8d2ae829dabc09bfc2b
AndresCastilloPlata <plataandres26@gmail.com>
2024-11-20 10:49:38 -0500

    se realiza mapeo y ajuste para que filtre el posterpath de los elementos que no lo contienen y no muestre esas peliculas

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:film_maniac/config/constants/environment.dart';
 import 'package:film_maniac/domain/datasources/movies_datasource.dart';
 import 'package:film_maniac/domain/entities/movie.dart';
+import 'package:film_maniac/infrastructure/mappers/movie_mapper.dart';
+import 'package:film_maniac/infrastructure/models/moviedb/moviedb_response.dart';
 
 class MoviedbDatasource extends MoviesDatasource {
   final dio = Dio(BaseOptions(
@@ -16,7 +18,12 @@ class MoviedbDatasource extends MoviesDatasource {
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     final response = await dio.get('/movie/now_playing');
 
-    final List<Movie> movies = [];
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movies = movieDBResponse.results
+        .where((moviedb) => moviedb.posterPath != 'no-poster')
+        .map((moviedb) => MovieMapper.movieDBToEntity(moviedb))
+        .toList();
     return movies;
   }
 }
