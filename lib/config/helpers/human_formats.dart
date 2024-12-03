3ccf4a8b3fbe4b55809d45589e58c118bf1fbbe8
AndresCastilloPlata <plataandres26@gmail.com>
2024-12-03 08:53:03 -0500

    mostrar peliculas en la busqueda y su clasificacion, adicional se realiza ajuste del human format para que tenga solo un decimal

--- lib/config/helpers/human_formats.dart
+++ lib/config/helpers/human_formats.dart
@@ -2,10 +2,10 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 
 class HumanFormats {
-  static String number(double number) {
-    final formatterNumbre =
-        NumberFormat.compactCurrency(decimalDigits: 0, symbol: '', locale: 'en')
-            .format(number);
+  static String number(double number, [int decimals = 0]) {
+    final formatterNumbre = NumberFormat.compactCurrency(
+            decimalDigits: decimals, symbol: '', locale: 'en')
+        .format(number);
     return formatterNumbre;
   }
 
