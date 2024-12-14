d13c8153f6eeb53f215898ed9f2520bf95ac2eab
AndresCastilloPlata <plataandres26@gmail.com>
2024-12-14 10:13:51 -0500

    se realiza ajuste en domain e infrastructure para la creacion del datasource y el repositorio

---
+++ lib/infrastructure/datasources/isar_datasource.dart
@@ -0,0 +1,22 @@
+import 'package:film_maniac/domain/datasources/local_storage_datasource.dart';
+import 'package:film_maniac/domain/entities/movie.dart';
+
+class IsarDatasource extends LocalStorageDatasource {
+  @override
+  Future<bool> isMovieFavorite(int movieId) {
+    // TODO: implement isMovieFavorite
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
+    // TODO: implement loadMovies
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> toggleFavorite(Movie movie) {
+    // TODO: implement toggleFavorite
+    throw UnimplementedError();
+  }
+}
