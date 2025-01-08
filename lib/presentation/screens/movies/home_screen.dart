1a785c4489c403c833d353eba4ae915c1a0c5868
AndresCastilloPlata <plataandres26@gmail.com>
2025-01-08 11:32:31 -0500

    se ajusta home screen para que mantenga el estado en que quedan las peliculas y se realiza el reemplazo de categoria a popular para la segunda vista

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -3,26 +3,61 @@ import 'package:flutter/material.dart';
 import 'package:film_maniac/presentation/views/views.dart';
 import 'package:film_maniac/presentation/widgets/widgets.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const name = 'home-screen';
   final int pageIndex;
 
-  const HomeScreen({super.key, required this.pageIndex});
+  const HomeScreen({
+    super.key,
+    required this.pageIndex,
+  });
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with AutomaticKeepAliveClientMixin {
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(keepPage: true);
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
 
   final viewRoutes = const <Widget>[
     HomeView(),
-    SizedBox(),
+    PopularView(),
     FavoritesView(),
   ];
 
   @override
   Widget build(BuildContext context) {
+    super.build(context);
+
+    if (pageController.hasClients) {
+      pageController.animateToPage(widget.pageIndex,
+          duration: const Duration(milliseconds: 250), curve: Curves.easeInOut);
+    }
+
     return Scaffold(
-      body: IndexedStack(
-        index: pageIndex,
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: pageController,
         children: viewRoutes,
       ),
-      bottomNavigationBar: CustomBottomNavigationbar(currentIndex: pageIndex),
+      bottomNavigationBar:
+          CustomBottomNavigationbar(currentIndex: widget.pageIndex),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
